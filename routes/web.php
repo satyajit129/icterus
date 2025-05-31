1ed571d4546e77d39cbb6da09c310bf4c3516676
Satyajit Roy <aitlsatyajit@gmail.com>
2025-05-31 19:17:33 +0600

    test commit from office

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,6 @@ Route::prefix('admin')->group(function () {
         Route::get('/settings',[AdminController::class,'adminSettings'])->name('adminSettings');
         Route::post('/settings-update',[AdminController::class,'adminSettingsUpdate'])->name('adminSettingsUpdate');
         Route::get('/logout', [AuthController::class, 'adminLogout'])->name('adminLogout');
-
-
         Route::get('designation', [AdminController::class, 'adminDesignation'])->name('adminDesignation');
     });
 
